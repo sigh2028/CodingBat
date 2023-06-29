dd1cc49eff205cec8c5ed9378139ef53f2df868c
Riyad <riyad.m.sh@gmail.com>
2023-06-29 14:53:39 -0400

    warmup1-10

---
+++ Java/Warmup1-10.java
@@ -0,0 +1,6 @@
+//Question: Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+//Answer: public String missingChar(String str, int n) {
+  String front = str.substring (0,n);
+  String end = str.substring (n+1);
+  return front + end;
+}
